653cf1971de28e6e5b8cb9814b7de0f5b93db95b
Ali Ashraf‬ <147933300+Ali-Ashraf-510@users.noreply.github.com>
2025-04-22 15:25:11 +0200

    Add new hotel entries to the database with detailed information

--- SQL queries/insert.sql
+++ SQL queries/insert.sql
@@ -12,5 +12,20 @@ VALUES ('WiFi', 'High-speed internet'), ('Pool', 'Outdoor swimming pool'), ('Gym
 INSERT INTO Hotels (name, location, description, price_per_night, image_url, star_rating, total_rooms, contact_number, email)
 VALUES ('Grand Hotel', 'Downtown', 'Luxury hotel with great views', 150.00, 'https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=414x232&ar=16x9', 5, 100, '1234567890', 'contact@grandhotel.com');
 
-INSERT INTO Hotel_Amenities (hotel_id, amenity_id)
-VALUES (1, 1), (1, 2);
+
+
+
+INSERT INTO Hotels (name, location, description, price_per_night, image_url, star_rating, total_rooms, contact_number, email)
+VALUES ('Ocean Breeze Resort', 'Beachfront', 'Relaxing resort with ocean views', 200.00, 'https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=', 4, 80, '9876543210', 'info@oceanbreezeresort.com');
+
+
+
+
+INSERT INTO Hotels (name, location, description, price_per_night, image_url, star_rating, total_rooms, contact_number, email)
+VALUES ('Cityscape Inn', 'City Center', 'Modern hotel for business travelers', 120.00, 'https://www.hilton.com/im/en/AUAJMES/19122401/auajm-poolsunset.jpg?impolicy=crop&cw=5000&ch=3333&gravity=NorthWest&xposition=0&yposition=1&rw=1280&rh=854', 3, 50, '5551234567', 'book@cityscapeinn.com');
+
+
+
+INSERT INTO Hotels (name, location, description, price_per_night, image_url, star_rating, total_rooms, contact_number, email)
+VALUES ('Mountain Retreat', 'Hillside', 'Cozy retreat with mountain views', 180.00, 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea-with-umbrella-chair-around-swimming-pool_74190-1072.jpg', 4, 60, '4449876543', 'reservations@mountainretreat.com');
+
